ced0671a63e39ddfad48e2720c5bc89078fa52da
HenryPC <fanghaof@sfu.ca>
2020-02-20 17:27:13 -0800

    Finished main game activity, waiting for minor improvements

--- app/src/main/java/com/example/cmpt276_a3/MainActivity.java
+++ app/src/main/java/com/example/cmpt276_a3/MainActivity.java
@@ -1,57 +1,53 @@
 package com.example.cmpt276_a3;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//hide state bar
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN); //hide state bar
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+
+        setupStartGameActivityButton();
+    }
+
+    private void setupStartGameActivityButton() {
+        Button button = findViewById(R.id.startButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = MainGamePlayActivity.makeIntentForMainGameActivity(MainActivity.this);
+                startActivity(intent);
+            }
+        });
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
 //
-//        View decorView = getWindow().getDecorView();
-//        // Hide the status bar.
-//        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-//        decorView.setSystemUiVisibility(uiOptions);
-//        // Remember that you should never show the action bar if the
-//        // status bar is hidden, so hide that too if necessary.
-//        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-//        actionBar.hide();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
 
--- app/src/test/java/com/example/cmpt276_a3/cmpt276_a3_model/Mines_ManagerTest.java
+++ app/src/test/java/com/example/cmpt276_a3/cmpt276_a3_model/Mines_ManagerTest.java
@@ -1,16 +1,33 @@
 package com.example.cmpt276_a3.cmpt276_a3_model;
 
+import java.util.Random;
+
 class Mines_ManagerTest {
     Mines_Manager mines_manager = Mines_Manager.getInstance();
 
     @org.junit.jupiter.api.Test
     void display2DArrayTest(){
-        mines_manager.setMineProperties(5, 5, 6);
-        mines_manager.generateNewMines();
+        int randomMines;
+        int currentRow = mines_manager.getRow();
+        int currentCol = mines_manager.getColumn();
+        Random random = new Random();
 
+        mines_manager.generateNewMines();
         mines_manager.display2DArray();
         System.out.println("---------------------------------------------");
-        mines_manager.checkMineAndReturnNewMine(3,1);
+
+        while(true){
+            randomMines = random.nextInt(currentRow * currentCol);
+            int randomRow = randomMines / currentCol;
+            int randomCol = randomMines / currentRow;
+
+            if(mines_manager.isGold(randomRow, randomCol)){
+                mines_manager.updateMine(randomRow, randomCol);
+                System.out.println("Row and Col is: " + randomRow + ", " + randomCol);
+                break;
+            }
+        }
+
         mines_manager.display2DArray();
     }
 
